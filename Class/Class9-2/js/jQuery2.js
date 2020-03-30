d4996a5840a616d91fb06512579395420448f8b4
Jose Guzman <disegnopp@gmail.com>
2020-03-30 16:25:52 -0400

    Class10

--- Class/Class9-2/js/jQuery2.js
+++ Class/Class9-2/js/jQuery2.js
@@ -2,39 +2,34 @@ $(document).ready( function(){
 
 
 $("#_hide").click(function(){
-  $(".gridContent div:first-child p").hide();
+  $(".gridContent div:first-child p").hide(3000);
 });
 
 $("#_show").click(function(){
-  $(".gridContent div:first-child p").show();
+  $(".gridContent div:first-child p").show(3000);
 });
 
 $("#_toggle").click( function(){
   $(".gridContent div:nth-child(2) p").toggle();
 });
 
+
 $("#_html").click(function() {
-  $(".gridContent div:nth-child(3) p").html("Hello <b>World</b>!");
+  $(".gridContent div:nth-child(3) p").html("Hello <strong>World</strong>");
 });
 
 $("#_css").click(function(){
-  $(".box").css("background-color", "lightblue");
+  $(".box").css({"background-color": "lightblue", "width": "100px", "height": "100px"});
 });
 
 
 $("footer").mouseover(function(){
-    $("footer p").html("Yeah! You're over me");
+  $("footer p").html("Yeah, it's over me");
 });
 
-$("footer").mouseleave(function() {
-  $("footer p").html("Comeback...=(");
+$("footer").mouseleave(function(){
+  $("footer p").html("Don't go!!");
 });
-
-
-
-// --------------------- Debug #4 ----------------------------
-
-// Here goes your jQuery :
 
 
 
